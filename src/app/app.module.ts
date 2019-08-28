52859c0958455fb05acdd6f9fd9de46e44343b99
Lauren Mooney <49599773+laurennmooney@users.noreply.github.com>
2019-08-28 00:11:30 -0400

    components created, routes written

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { QuizService } from './quiz.service';
 import { AppComponent } from './app.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { ResultsComponent } from './results/results.component';
+import { ScoresComponent } from './scores/scores.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: "quiz", component: QuizComponent },
+  { path: "results", component: ResultsComponent },
+  { path: "scores", component: ScoresComponent },
+  { path: "", redirectTo: "quiz" }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizComponent,
+    ResultsComponent,
+    ScoresComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [QuizService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
